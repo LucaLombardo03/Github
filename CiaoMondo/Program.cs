5858abc6caea7287b668fe412229b1ef8b25e731
luca lombardo <lucalombardo03@gmail.com>
2019-10-03 18:43:42 +0200

    lingua italiana

---
+++ CiaoMondo/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CiaoMondo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string risposta;
+            Console.WriteLine("Sei italiano o inglese?");
+            risposta = Console.ReadLine();
+            if (risposta == "italiano")
+                Console.WriteLine("Ciao Mondo");
+            else if (risposta == "inglese")
+                Console.WriteLine("Hello World!");
+            Console.ReadLine();
+
+        }
+    }
+}
